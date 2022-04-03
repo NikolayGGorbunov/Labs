569342157c2ca896b0fe7747f672814250c6e4bc
Nikolay Gorbunov <gorbunov.nv13@gmail.com>
2022-04-03 08:01:03 +0300

    Added tests for task1 and task2

--- lab-for-5th-lec/task2/task2.rb
+++ lab-for-5th-lec/task2/task2.rb
@@ -1,12 +1,22 @@
-def read_students(file_name)
-  file = File.open(file_name)
+require 'stringio'
+
+def read_students(filename_students)
+  file = File.open(filename_students)
   students_list = file.readlines.map(&:chomp)
   file.close
   students_list
 end
 
-def write_students(students, age)
-  file = File.open('result.txt', 'a')
+def write_students(students, age, filename_result)
+  loop do
+    if age.length != 2
+       puts 'Age must be between 10 and 99'
+       age = $stdin.gets.chomp
+    else
+      break
+    end
+  end
+  file = File.open(filename_result, 'a')
   written_students = Array.new
   students.map do |student|
     if student.include?(age)
@@ -18,9 +28,67 @@ def write_students(students, age)
   students - written_students
 end
 
-File.delete('result.txt') if File.exist?('result.txt')
-students = read_students('students.txt')
-while !students.empty?
-  students = write_students(students, gets.chomp)
+def sort_students(filename_students, filename_result)
+  File.delete(filename_result) if File.exist?(filename_result)
+  students = read_students(filename_students)
+  start_len = students.length
+  final_len = 0
   puts students
+  puts 'Enter the age of the student'
+  while !students.empty?
+    students = write_students(students, $stdin.gets.chomp, filename_result)
+    final_len += 1
+    puts '++++++++++++++++++++++++++++'
+    puts students
+    puts 'Enter the age of the student'
+  end
+  [students.empty?, final_len == start_len]
+end
+
+#sort_students('students.txt', 'result.txt')
+
+#-------------------TESTS-------------------
+require 'rspec'
+
+RSpec.describe 'read_students() method' do
+  before:all do
+    File.delete('teststudents.txt') if File.exist?('teststudents.txt')
+    File.write('teststudents.txt', "Иван Иванов 20\nПетр Петров 23\n")
+  end
+  after:all do
+    File.delete('teststudents.txt')
+  end
+
+  it 'puts students from file and return array of students' do
+    expect(read_students('teststudents.txt')).to eq(['Иван Иванов 20', 'Петр Петров 23'])
+  end
+end
+
+RSpec.describe 'write_students() method' do
+  before:all do
+    @students = ['Иван Иванов 20', 'Петр Петров 23']
+  end
+  after:all do
+    File.delete('testresult.txt')
+  end
+
+  it "takes the student's age and moves all students with that age to the result file. Returns the remaining students." do
+    expect(write_students(@students, '23', 'testresult.txt')).to eq(['Иван Иванов 20'])
+  end
+end
+
+RSpec.describe 'sort_students() method' do
+  before:all do
+    File.delete('teststudents.txt') if File.exist?('teststudents.txt')
+    File.write('teststudents.txt', "Иван Иванов 20\nПетр Петров 23\n")
+  end
+  after:all do
+    File.delete('teststudents.txt')
+    File.delete('testresult.txt')
+  end
+
+  it "rewrites students from a given file into a new file in a given order with respect to age." do
+    allow($stdin).to receive(:gets).and_return('23', '20')
+    expect(sort_students('teststudents.txt', 'testresult.txt')).to eq([true, true])
+  end
 end
--- lab-for-5th-lec/task3/task3.rb
+++ lab-for-5th-lec/task3/task3.rb
@@ -1,52 +1,83 @@
+require 'stringio'
 DEFAULT_BALANCE = 100.0
 
-def init()
-  File.exist?('balance.txt') ? File.read('balance.txt') : DEFAULT_BALANCE
+def init(filename)
+  if File.exist?(filename)
+    File.read(filename).to_f
+  else
+    File.write(filename, DEFAULT_BALANCE)
+    DEFAULT_BALANCE
+  end
 end
 
 def deposit(balance, value)
-  new_balance = (balance.to_f + value.to_f).to_s
+  new_balance = (balance + value)
 end
 
 def withdraw()
-  new_balance = (balance.to_f - value.to_f).to_s
+  new_balance = (balance - value)
 end
 
 def show_balance(balance)
   puts balance
 end
 
-def quit(balance)
-  File.write('balance.txt', balance)
+def quit(filename, balance)
+  File.write(filename, balance)
 end
 
-balance = init()
+balance = init('balance.txt')
 loop do
   puts "Please choose action\nB: Show balance | D: Deposit | W: Withdraw | Q: Quit"
-  case gets.chomp
+  case $stdin.gets.chomp
   when 'Q', 'q'
-    quit(balance)
+    quit('balance.txt', balance)
     break
   when 'B', 'b'
     show_balance(balance)
   when 'D', 'd'
     puts 'How much do you want to deposit?'
-    value = gets.chomp
-    if value.to_f > 0.0
+    value = gets.chomp.to_f
+    if value > 0.0
       balance = deposit(balance, value)
     else
       puts "You can't deposit negative or zero value. Try again."
     end
   when 'W', 'w'
     puts 'How much do you want to withdraw?'
-    value = gets.chomp
-    if value.to_f <= 0.0
+    value = gets.chomp.to_f
+    if value <= 0.0
       puts "You can't withraw negative or zero value. Try again"
-    elsif value.to_f > balance
+    elsif value > balance
       puts "You can't withdraw more than your balance. Try again"
     else
       balance = withdraw(balance, value)
+    end
   else
     puts 'Error: invalid value. Try again.'
+  end
+end
+
+#-------------------TESTS-------------------
+#Тесты здесь только на init() потому что остальные методы это стандартные операции
+#Тесты на варианта банкомата с классами будут более полные потому что эта реализация в целом неудобная
+require 'rspec'
+
+RSpec.describe 'init() method' do
+  before:all do
+    File.delete('testbalance.txt') if File.exist?('testbalance.txt')
+    File.write('testbalance.txt', 1999.0)
+  end
+  after:all do
+    File.delete('testbalance.txt')
+    File.delete('default_balance.txt')
+  end
+
+  it 'create new balance file with default value and return balance' do
+    #allow($stdin).to receive(:gets).and_return('q')
+    expect(init('default_balance.txt')).to eq(100.0)
+  end
+  it 'read balance from file and return it' do
+    expect(init('testbalance.txt')).to eq(1999.0)
   end
 end
