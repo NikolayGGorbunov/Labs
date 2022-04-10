8a58ef9e9293d3b31dbdaf71ee63faaa27e7f069
Nikolay Gorbunov <gorbunov.nv13@gmail.com>
2022-04-10 08:49:56 +0300

    Added HTTP-CashMachine

--- lab-for-10th-lec/http_srv.rb
+++ lab-for-10th-lec/http_srv.rb
@@ -3,22 +3,38 @@ require 'rack'
 
 server = TCPServer.new('localhost', 3000)
 
-class App
+class CashMachine
+  @@balance = 100.0
+
   def call(env)
-      [200, {"Content_Type" => "text/plain"}, ["Hello World"]]
+      case env["PATH_INFO"]
+      when '/balance'
+        [200, {'Content_Type' => 'text/plain'}, ["Your balance is: #{@@balance}"]]
+      when '/deposit'
+        value = env['OPT'].split('=')[1].to_i
+        @@balance += value
+        [200, {'Content_Type' => 'text/plain'}, ["Your balance incresed! Current balance is: #{@@balance}"]]
+      when '/withdraw'
+        value = env['OPT'].split('=')[1].to_i
+        @@balance -= value
+        [200, {'Content_Type' => 'text/plain'}, ["Your balance decresed! Current balance is: #{@@balance}"]]
+      else
+        [400, {'Content_Type' => 'text/plain'}, ["Something went wrong\n", "#{env}"]]
+      end
   end
 end
 
-app = App.new
+http_machine = CashMachine.new
 
 while connection = server.accept
   request = connection.gets
   method, full_path = request.split(' ')
-  path = full_path.split('?')[0]
+  path, option = full_path.split('?')
 
-  status, headers, body = app.call({
+  status, headers, body = http_machine.call({
     'REQUEST_METHOD' => method,
-    'PATH_INFO' => path
+    'PATH_INFO' => path,
+    'OPT' => option
     })
 
   connection.print("HTTP/1.1 #{status}\r\n")
