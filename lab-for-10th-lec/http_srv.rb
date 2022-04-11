f230a6b4567108cea19b6bfcdc7f244ae91fb17e
Nikolay Gorbunov <gorbunov.nv13@gmail.com>
2022-04-11 08:22:23 +0300

    minor changes

--- lab-for-10th-lec/http_srv.rb
+++ lab-for-10th-lec/http_srv.rb
@@ -1,8 +1,6 @@
 require 'socket'
 require 'rack'
 
-server = TCPServer.new('localhost', 3000)
-
 class CashMachine
   @@balance = 100.0
 
@@ -24,26 +22,31 @@ class CashMachine
   end
 end
 
-http_machine = CashMachine.new
+def server_init
+  server = TCPServer.new('localhost', 3000)
+  http_machine = CashMachine.new
 
-while connection = server.accept
-  request = connection.gets
-  method, full_path = request.split(' ')
-  path, option = full_path.split('?')
+  while connection = server.accept
+    request = connection.gets
+    method, full_path = request.split(' ')
+    path, option = full_path.split('?')
 
-  status, headers, body = http_machine.call({
-    'REQUEST_METHOD' => method,
-    'PATH_INFO' => path,
-    'OPT' => option
-    })
+    status, headers, body = http_machine.call({
+      'REQUEST_METHOD' => method,
+      'PATH_INFO' => path,
+      'OPT' => option
+      })
 
-  connection.print("HTTP/1.1 #{status}\r\n")
-  headers.each do |key, value|
-    connection.print("#{key}: #{value}\r\n")
+    connection.print("HTTP/1.1 #{status}\r\n")
+    headers.each do |key, value|
+      connection.print("#{key}: #{value}\r\n")
+    end
+    connection.print "\r\n"
+    body.each do |part|
+      connection.print(part)
+    end
+    connection.close
   end
-  connection.print "\r\n"
-  body.each do |part|
-    connection.print(part)
-  end
-  connection.close
 end
+
+server_init
